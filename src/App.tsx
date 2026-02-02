06c2036341371839afd6f9d54afe547445f2ba96
udara88 <udarawickramasinghe88@gmail.com>
2026-02-02 10:02:04 +0530

    Add paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,19 @@ const App = () => {
       <h1>Hello React</h1>
       <p>This is a test automation deployment trigger.</p>
       <h2>This is a new change</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit ipsam
+        deserunt cum sapiente. Nulla tempora suscipit consequuntur error
+        deserunt quia, reiciendis hic optio consectetur beatae omnis eius non
+        quam! Ipsam officiis eaque repellendus consectetur doloremque ratione
+        soluta unde? Pariatur voluptatibus inventore ad tempora similique
+        officiis, fugiat voluptatum libero maxime, nulla est laboriosam minima
+        fugit temporibus nemo, neque explicabo dolorum dolores provident velit.
+        Nesciunt blanditiis magnam necessitatibus quos maiores, perferendis
+        itaque, a asperiores illum officia alias unde dicta architecto eveniet
+        est dolores accusantium ab rem error ratione maxime! Dolor odio quisquam
+        rem accusantium, totam ipsum neque aliquam rerum tempore. Odit, quidem.
+      </p>
     </>
   );
 };
